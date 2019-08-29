11ef05a023bbbdb93c913d9ba282407b61c1e589
maldhi <maldhi255@gmail.com>
2019-08-29 21:54:02 +0700

    Initial commit

---
+++ percepatan/Assets/rotTransl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotTransl : MonoBehaviour {
+
+	 public float moveSpeed = 10f;
+    public float turnSpeed = 50f;
+    
+    
+    void Update ()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        
+        if(Input.GetKey(KeyCode.DownArrow))
+            transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+        
+        if(Input.GetKey(KeyCode.LeftArrow))
+            transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+        
+        if(Input.GetKey(KeyCode.RightArrow))
+            transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+    }
+}
